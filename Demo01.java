e421d826c8a47e66a098d403061c56ed28313c01
bobobo00 <48381156+bobobo00@users.noreply.github.com>
2019-12-14 22:11:27 +0800

    字节码操作-Javassist
    4，Javassist：编辑和创建Java字节码的类库。可以使用源代码（java）也可以使用JVM底层操作指令。
    操作：
    创建一个全新的类。
    使用XJAD反编译工具，将生成的class文件反编译成java文件。

---
+++ Demo01.java
@@ -0,0 +1,41 @@
+package Javassist;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtConstructor;
+import javassist.CtField;
+import javassist.CtMethod;
+
+/**
+ * 测试使用jvassist生成一个类。
+ * @author dell
+ *
+ */
+public class Demo01 {
+	public static void main(String[] args) throws Exception {
+		ClassPool pool=ClassPool.getDefault();
+		CtClass cc=pool.makeClass("TestJavassist.Emp");
+		
+		//创建属性
+		CtField f1=CtField.make("private int empno;",cc);
+		CtField f2=CtField.make("private String ename;", cc);
+		cc.addField(f1);
+		cc.addField(f2);
+		
+		//创建方法
+		CtMethod method1=CtMethod.make("public int getEmpno(){return empno;}", cc);
+		CtMethod method2=CtMethod.make("public void setEmpno(String ename){this.ename=ename;}",cc);
+		cc.addMethod(method1);
+		cc.addMethod(method2);
+		
+		//添加构造器
+		CtConstructor constructor=new CtConstructor(new CtClass[] {CtClass.intType,pool.get("java.lang.String")},cc);
+		constructor.setBody("{this.empno=empno;this.ename=ename;}");
+		cc.addConstructor(constructor);
+		
+		cc.writeFile("E:/javac");//将写好的类写入到E：/javac中
+		System.out.println("成功，生成类");
+		
+		
+	}
+}
